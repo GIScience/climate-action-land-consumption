216f4f5dd34e182850a614f3983c2f659983e0db
Sebastián Block <sebastian.block@heigit.org>
2024-11-19 15:59:06 +0000

    Resolve "Add parking lots as a land consumption class"

--- test/test_calculation.py
+++ test/test_calculation.py
@@ -6,14 +6,14 @@ from land_consumption.calculation import calculate_land_consumption
 def test_calculate_land_consumption():
     expected_output = pd.DataFrame(
         {
-            'Land Use': ['Buildings', 'Unknown consumption'],
-            'Total Land Area [ha]': [0.25, 0.75],
-            '% Land Area': [25.0, 75.0],
-            'Consumption Factor': [1.0, None],
-            '% Land Consumed by known classes': [25.0, None],
+            'Land Use': ['Buildings', 'Parking lots', 'Unknown consumption'],
+            'Total Land Area [ha]': [0.25, 0.10, 0.65],
+            '% Land Area': [25.0, 10.0, 65.0],
+            'Consumption Factor': [1.0, 0.8, None],
+            '% Land Consumed by known classes': [25.0, 8.0, None],
         }
     )
 
-    computed_output = calculate_land_consumption(aoi_area=10000.0, building_area=2500)
+    computed_output = calculate_land_consumption(aoi_area=10000.0, building_area=2500, parking_area=1000)
 
     pd.testing.assert_frame_equal(computed_output, expected_output)
