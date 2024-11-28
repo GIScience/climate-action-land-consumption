255158542f6db7d3c28b08fc6b431917817cac72
Sebastián Block <sebastian.block@heigit.org>
2024-11-28 15:52:04 +0000

    dep: update climatooloogy to 6.0.1

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,14 +1,26 @@
-def test_plugin_info_request(operator, expected_info_output):
-    assert operator.info() == expected_info_output
+from climatoology.base.info import _Info
+from climatoology.base.artifact import _Artifact
+
+
+def test_plugin_info_request(operator):
+    assert isinstance(operator.info(), _Info)
 
 
 def test_plugin_compute_request(
-    operator, expected_compute_input, expected_compute_output, compute_resources, request_ohsome
+    operator,
+    expected_compute_input,
+    compute_resources,
+    request_ohsome,
+    default_aoi,
+    default_aoi_properties,
 ):
-    assert (
-        operator.compute(
-            resources=compute_resources,
-            params=expected_compute_input,
-        )
-        == expected_compute_output
+    computed_artifacts = operator.compute(
+        resources=compute_resources,
+        aoi=default_aoi,
+        aoi_properties=default_aoi_properties,
+        params=expected_compute_input,
     )
+
+    assert len(computed_artifacts) == 1
+    for artifact in computed_artifacts:
+        assert isinstance(artifact, _Artifact)
